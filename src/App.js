893204a350b667810887bff004274268cb90cb75
Andre Eli <aeli@hot.ee>
2022-06-25 00:10:13 +0300

    Date komponent tõstetud eraldi faili.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App() {
 
   const expenses = [
     { title: 'Auto kindlustus', amount: 165.50, date: new Date(2022, 2, 28) },
-    { title: 'Söök', amount: 28.50, date: new Date(2022, 1, 18) },
+    { title: 'Söök', amount: 28.50, date: new Date(2022, 0, 18) },
     { title: 'Meelelahutus', amount: 225.00, date: new Date(2021, 8, 20) },
-    { title: 'Kohting', amount: 75.68, date: new Date(2022, 8, 10) },
+    { title: 'Kohting', amount: 75.68, date: new Date(2022, 7, 10) },
   ];
 
   return (
     <div>
       <header>
         <p>
-          <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-          <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-          <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-          <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+          <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+          <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+          <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+          <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
         </p>
       </header>
     </div>
