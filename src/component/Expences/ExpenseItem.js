a7a8e359298f4bd67208a05eb44563a003c21509
Andre Eli <aeli@hot.ee>
2022-06-29 20:50:26 +0300

    Lisatud nupp ja clickHandler.

--- src/component/Expences/ExpenseItem.js
+++ src/component/Expences/ExpenseItem.js
@@ -4,13 +4,20 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
+    let title = props.title;
+
+    function clickHandler() {
+        title = 'Uus pealkiri'
+    };
+
     return( 
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount} €</div>
             </div>
+            <button onClick={clickHandler}>Vajuta</button>
         </Card>
     );
 }
