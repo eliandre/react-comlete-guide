c2cdc6be1b020370243f89ecd1df0cb91c3fc922
Andre Eli <aeli@hot.ee>
2022-07-10 20:31:52 +0300

    Lisatud näitena ühe oleku kasutamine.

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -4,48 +4,51 @@ import './ExpenseForm.css';
 function ExpenseForm() {
 
     // 1. variant - kasutada mitut state'i.
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     // 2. variant - kasutada üht state'i. Väärtusena antakse edasi objekt, mitte sõne nagu esimeses variandis.
     // Vahe on selles, et uuendatakse kogu kolme omadust.
     // Mõlemad variandid on samaväärsed ja pole vahet kumba kasutada.
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enetredAmount: '',
-        enteredDate: ''
-    });
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enetredAmount: '',
+    //     enteredDate: ''
+    // });
 
     // Näitab, milline väärtus on kasutaja poolt sisestatud ja see salvestatakse
     function titleChangeHandler(event) {
         // 1. variant
-        //setEnteredTitle(event.target.value);
+        setEnteredTitle(event.target.value);
         // 2. variant
-        setUserInput({
+        // setUserInput({
             // spread operaator (...), et ülejäänud omadused kaasa võetaks, muidu jäävad tühjaks. See võtab objekti,
             // tõmbab kõik võti:väärtus paarid välja ning lisab uude objekti
-            ...userInput,
+            // ...userInput,
             // ja siis saame võti:väärtus paarid vajadusel üle kirjutada
-            enteredTitle: event.title.value,
-        });
+            // enteredTitle: event.title.value,
+        // });
+        // 2. variant pole siiski hea lahendus, kuna sõlub eelmisest olekust. Parandus: funktsioonile anda edasi
+        // teine funktsioon. Võetakse eelmise oleku (prevState) snapshot ja tagastatakse uue oleku snapshot.
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value }
+        // });
     }
 
     function amountChangeHandler(event) {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.title.value,
-        });
+        setEnteredAmount(event.target.value);
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value }
+        // });
     }
 
     function dateChangeHandler(event) {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.title.value,
-        });
+        setEnteredDate(event.target.value);
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value }
+        // });
     }
 
     return <form>
